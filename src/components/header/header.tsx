609fc5c0e0c207460c0114430281a37b16532ef3
hawkstein <liamhawks+liamhawks@gmail.com>
2021-01-17 14:01:24 +0000

    feat: add links to main menu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,15 +6,15 @@ function Header() {
   const [hover, setHover] = useState(false)
   return (
     <>
-      <header className={`container ${styles.header}`}>
+      <header className={`container mt-3`}>
         <div className="row align-items-center">
           <div className="col col-xl">
             <div>Header Graphic</div>
           </div>
           <div className={styles.headerLinks} role="menu">
             <div
-              className="hoverWrapper"
-              role="menuitem"
+              className="hoverWrapper mr-4"
+              role="button"
               onMouseEnter={() => {
                 setHover(true)
               }}
@@ -33,8 +33,11 @@ function Header() {
             <a className="d-none d-lg-inline-block" href="/topic-three">
               Topic Three
             </a>
-            <a className="d-none d-lg-inline-block" href="/topic-four">
-              Topic Four
+            <a
+              className="btn btn-dark d-none d-lg-inline-block"
+              href="/topic-four"
+            >
+              Call to Action
             </a>
           </div>
         </div>
@@ -43,11 +46,40 @@ function Header() {
         className={`${hover ? `${styles.dropdownMenuHovered} ` : ""}${
           styles.dropdownMenu
         }`}
+        onMouseEnter={() => {
+          setHover(true)
+        }}
+        onMouseLeave={() => {
+          setHover(false)
+        }}
       >
         <div className="row">
-          <div className="col-4">Column One</div>
-          <div className="col-4">Column Two</div>
-          <div className="col-4">Column Three</div>
+          <div className="col-auto">
+            <a className="d-block mb-3">Link One</a>
+            <a className="d-block mb-3">Link Two</a>
+            <a className="d-block mb-3">Link Three</a>
+            <a className="d-block mb-3">Link Four</a>
+            <a className="d-block mb-3">Link Five</a>
+          </div>
+          <div className="col-auto">
+            <a className="d-block mb-3">Link One</a>
+            <a className="d-block mb-3">Link Two</a>
+            <a className="d-block mb-3">Link Three</a>
+            <a className="d-block mb-3">Link Four</a>
+            <a className="d-block mb-3">Link Five</a>
+          </div>
+          <div className="col-auto">
+            <a className="d-block mb-3">Link One</a>
+            <a className="d-block mb-3">Link Two</a>
+            <a className="d-block mb-3">Link Three</a>
+            <a className="d-block mb-3">Link Four</a>
+            <a className="d-block mb-3">Link Five</a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <a className="btn btn-info btn-block">Menu Call To Action</a>
+          </div>
         </div>
       </div>
     </>
