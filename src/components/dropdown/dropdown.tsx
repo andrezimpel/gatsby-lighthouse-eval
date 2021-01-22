e165ba3874ee3a045a3377f662e5d76d88ea1085
hawkstein <liamhawks+liamhawks@gmail.com>
2021-01-22 17:25:21 +0000

    feat: improve Dropdown

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -1,39 +1,56 @@
 import * as React from "react"
 import { useState } from "react"
 import * as styles from "./dropdown.module.scss"
+import useWindowSize from "../../hooks/useWindowSize"
 
-function Dropdown({ children }) {
+function Arrow() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      style={{
+        marginBottom: "4px",
+        marginLeft: "4px",
+        transform: "rotate(90deg)",
+      }}
+    >
+      <g fill="none" fillRule="evenodd">
+        <path
+          stroke="#fff"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M8 15l5-5h0L8 5"
+        ></path>
+      </g>
+    </svg>
+  )
+}
+
+function Dropdown({ label, children }) {
   const [isExpanded, setExpanded] = useState(false)
+  const windowSize = useWindowSize()
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <button
-          className="btn btn-dark"
-          onMouseEnter={() => {
-            setExpanded(true)
-          }}
-          onMouseLeave={() => {
-            setExpanded(false)
-          }}
-          onClick={() => {
-            setExpanded((expanded) => !expanded)
-          }}
-        >
-          Hover/Click
-        </button>
-        <div
-          className={`${styles.dropdown} ${
-            isExpanded ? `${styles.expanded}` : ""
-          }`}
-          onMouseEnter={() => {
-            setExpanded(true)
-          }}
-          onMouseLeave={() => {
-            setExpanded(false)
-          }}
-        >
-          {children}
-        </div>
+      <button
+        className="btn btn-dark"
+        onClick={() => {
+          console.log(windowSize)
+          if (windowSize.width < 992) {
+            setExpanded((v) => !v)
+          }
+        }}
+      >
+        {label}
+        <Arrow />
+      </button>
+      <div
+        className={`${styles.content}${
+          isExpanded ? ` ${styles.expanded}` : ""
+        }`}
+      >
+        {children}
       </div>
     </div>
   )
