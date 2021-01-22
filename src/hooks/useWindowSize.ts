d0ee7a26adf5c2fe600ca6c57fae87cf04a1cad6
hawkstein <liamhawks+liamhawks@gmail.com>
2021-01-22 17:33:42 +0000

    refactor: useWindowSize hook to TS

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,14 +1,21 @@
-import { useState, useEffect } from "react"
+import * as React from "react"
+
+type WindowDimensions = {
+  width: number | undefined
+  height: number | undefined
+}
+
+type SetWindowSizeState = React.Dispatch<React.SetStateAction<WindowDimensions>>
 
 function useWindowSize() {
-  const [windowSize, setWindowSize] = useState({
+  const [windowSize, setWindowSize] = React.useState<WindowDimensions>({
     width: undefined,
     height: undefined,
   })
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (typeof window !== "undefined") {
-      function handleResize() {
+      const handleResize = () => {
         setWindowSize({
           width: window.innerWidth,
           height: window.innerHeight,
