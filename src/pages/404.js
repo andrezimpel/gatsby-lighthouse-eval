0b81da081ff1971ba60d46eb8d99d241b3a1bcf6
hawkstein <liamhawks+liamhawks@gmail.com>
2021-01-24 14:21:23 +0000

    feat: style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Helmet } from "react-helmet"
-//import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/404.scss"
 
 const IndexPage = () => {
   return (
@@ -9,12 +9,16 @@ const IndexPage = () => {
         <html lang="en" />
         <title>404 - Not Found</title>
       </Helmet>
-      <header></header>
-      <main>
+      <main className="bg-dark w-100 h-100">
         <div className="container">
           <div className="row">
-            <div className="col-12 mt-5">
-              <h1>404: Not Found!</h1>
+            <div
+              className="col-12 d-flex justify-content-center align-items-center"
+              style={{ height: "100vh" }}
+            >
+              <h1 className="text-highlight display-4 font-weight-bold text-center pb-5">
+                Whoops! 404:&nbsp;Not&nbsp;Found!
+              </h1>
             </div>
           </div>
         </div>
